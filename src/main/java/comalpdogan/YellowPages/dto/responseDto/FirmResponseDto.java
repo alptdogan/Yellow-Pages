bf19ef422562162cfc06976246a543d1401834ed
Alptuğ Doğan <alpdogan000@gmail.com>
2023-01-12 12:12:19 +0300

    Create FirmResponseDto

---
+++ src/main/java/comalpdogan/YellowPages/dto/responseDto/FirmResponseDto.java
@@ -0,0 +1,12 @@
+package comalpdogan.YellowPages.dto.responseDto;
+
+import lombok.Data;
+
+@Data
+public class FirmResponseDto {
+
+    private int id;
+    private String firmName;
+    private int jobId;
+
+}
