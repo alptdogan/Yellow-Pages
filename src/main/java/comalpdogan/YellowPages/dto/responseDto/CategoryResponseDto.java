7d6f1429bfd1a9d786fd57eadc740e5f787a2200
Alptuğ Doğan <alpdogan000@gmail.com>
2023-01-12 12:12:34 +0300

    Create CategoryResponseDto

---
+++ src/main/java/comalpdogan/YellowPages/dto/responseDto/CategoryResponseDto.java
@@ -0,0 +1,12 @@
+package comalpdogan.YellowPages.dto.responseDto;
+
+import lombok.Data;
+
+@Data
+public class CategoryResponseDto {
+
+    private int id;
+    private String categoryName;
+    private int jobId;
+
+}
