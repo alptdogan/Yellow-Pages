b5e03fa9bb16c6cf7d4f6f25fc7d20a7235fe21e
Alptuğ Doğan <alpdogan000@gmail.com>
2023-01-12 12:14:48 +0300

    Create SaveJobRequestDto

---
+++ src/main/java/comalpdogan/YellowPages/dto/requestDto/SaveJobRequestDto.java
@@ -0,0 +1,12 @@
+package comalpdogan.YellowPages.dto.requestDto;
+
+import lombok.Data;
+
+@Data
+public class SaveJobRequestDto {
+
+    private String title;
+    private int firmId;
+    private int categoryId;
+
+}
