f065a901e3b0318cf6ea2df207fca52ca082ebc1
Alptuğ Doğan <alpdogan000@gmail.com>
2023-01-12 12:13:34 +0300

    Create UpdateCategoryRequestDto

---
+++ src/main/java/comalpdogan/YellowPages/dto/requestDto/UpdateCategoryRequestDto.java
@@ -0,0 +1,11 @@
+package comalpdogan.YellowPages.dto.requestDto;
+
+import lombok.Data;
+
+@Data
+public class UpdateCategoryRequestDto {
+
+    private int id;
+    private String categoryName;
+
+}
