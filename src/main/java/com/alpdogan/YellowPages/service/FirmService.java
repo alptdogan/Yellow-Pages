618dd66421810ec08b58644a5fe8857d91ece68d
Alptuğ Doğan <alpdogan000@gmail.com>
2023-01-14 15:20:18 +0300

    Update FirmService

--- src/main/java/com/alpdogan/YellowPages/service/FirmService.java
+++ src/main/java/com/alpdogan/YellowPages/service/FirmService.java
@@ -1,11 +1,14 @@
 package com.alpdogan.YellowPages.service;
 
+import com.alpdogan.YellowPages.dto.requestDto.UpdateFirmRequestDto;
 import com.alpdogan.YellowPages.entity.Firm;
 import com.alpdogan.YellowPages.dto.requestDto.SaveFirmRequestDto;
 import com.alpdogan.YellowPages.repository.FirmRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class FirmService {
@@ -31,6 +34,21 @@ public class FirmService {
 
         return firm.getFirmName() + " has been created successfully.";
 
+    }
+
+    public String updateFirm(UpdateFirmRequestDto updateFirmRequestDto)
+    {
+        int firmIdRequest = updateFirmRequestDto.getId();
+        String firmNameRequest = updateFirmRequestDto.getFirmName();
+
+        Optional<Firm> firmOptional = firmRepository.findById(firmIdRequest);
+        Firm firm = firmOptional.get();
+
+        firm.setFirmName(firmNameRequest);
+
+        firmRepository.save(firm);
+
+        return "Changes saved successfully.";
     }
 
 }
