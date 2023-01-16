b44d65388bce075b0bcf2e950a117aa126992ef5
Alptuğ Doğan <alpdogan000@gmail.com>
2023-01-16 13:07:01 +0300

    Update CategoryServiceTest

--- src/test/java/com/alpdogan/YellowPages/service/CategoryServiceTest.java
+++ src/test/java/com/alpdogan/YellowPages/service/CategoryServiceTest.java
@@ -1,11 +1,18 @@
 package com.alpdogan.YellowPages.service;
 
+import com.alpdogan.YellowPages.dto.requestDto.SaveCategoryRequestDto;
+import com.alpdogan.YellowPages.entity.Category;
 import com.alpdogan.YellowPages.repository.CategoryRepository;
 
+import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 
 @SpringBootTest
@@ -20,6 +27,23 @@ public class CategoryServiceTest {
     @Mock
     ModelMapper modelMapper;
 
+    @Test
+    void testSaveCategory() {
 
+        SaveCategoryRequestDto saveCategoryRequestDtoMock = mock(SaveCategoryRequestDto.class);
+        saveCategoryRequestDtoMock.setCategoryName("Software Developer");
+
+        Category categoryMock = mock(Category.class);
+        categoryMock.setId(1);
+
+        when(modelMapper.map(saveCategoryRequestDtoMock, Category.class)).thenReturn(categoryMock);
+        when(categoryRepository.save(categoryMock)).thenReturn(categoryMock);
+
+        String saveCategory = categoryService.saveCategory(saveCategoryRequestDtoMock);
+        String saveCategoryMessage = categoryMock.getCategoryName() + " has been created successfully.";
+
+        assertEquals(saveCategory, saveCategoryMessage);
+
+    }
 
 }
