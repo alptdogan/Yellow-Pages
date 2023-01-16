f6e8812cb197017e28acb35646fc78337085163c
Alptuğ Doğan <alpdogan000@gmail.com>
2023-01-16 13:29:39 +0300

    Update CategoryServiceTest

--- src/test/java/com/alpdogan/YellowPages/service/CategoryServiceTest.java
+++ src/test/java/com/alpdogan/YellowPages/service/CategoryServiceTest.java
@@ -1,6 +1,7 @@
 package com.alpdogan.YellowPages.service;
 
 import com.alpdogan.YellowPages.dto.requestDto.SaveCategoryRequestDto;
+import com.alpdogan.YellowPages.dto.requestDto.UpdateCategoryRequestDto;
 import com.alpdogan.YellowPages.entity.Category;
 import com.alpdogan.YellowPages.repository.CategoryRepository;
 
@@ -58,6 +59,40 @@ public class CategoryServiceTest {
         Category findCategory = categoryService.findCategory(categoryMock.getId());
 
         assertEquals(categoryMock, findCategory);
+
+    }
+
+    @Test
+    void testUpdateCategory() {
+
+        Category categoryMock = mock(Category.class);
+        categoryMock.setId(1);
+        categoryMock.setCategoryName("Software Developer");
+
+        UpdateCategoryRequestDto updateCategoryRequestDtoMock = mock(UpdateCategoryRequestDto.class);
+        updateCategoryRequestDtoMock.setCategoryName("Back End Developer");
+
+        when(categoryRepository.findById(categoryMock.getId())).thenReturn(Optional.of(categoryMock));
+
+        String updateCategory = categoryService.updateCategory(updateCategoryRequestDtoMock);
+        String updateCategoryMessage = "Changes saved successfully.";
+
+        assertEquals(updateCategory, updateCategoryMessage);
+
+    }
+
+    @Test
+    void testDeleteCategoryById() {
+
+        Category categoryMock = mock(Category.class);
+        categoryMock.setId(1);
+
+        when(categoryRepository.findById(categoryMock.getId())).thenReturn(Optional.of(categoryMock));
+
+        String deleteCategory = categoryService.deleteCategoryById(categoryMock.getId());
+        String deleteCategoryMessage = "The category has been deleted.";
+
+        assertEquals(deleteCategory, deleteCategoryMessage);
 
     }
 
