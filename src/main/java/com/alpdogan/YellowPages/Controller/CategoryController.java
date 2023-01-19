0fffaf5dd80971f007e20cbb12e9434c7800d25e
Alptuğ Doğan <alpdogan000@gmail.com>
2023-01-19 12:21:01 +0300

    Update CategoryController

--- src/main/java/com/alpdogan/YellowPages/Controller/CategoryController.java
+++ src/main/java/com/alpdogan/YellowPages/Controller/CategoryController.java
@@ -1,9 +1,16 @@
 package com.alpdogan.YellowPages.Controller;
 
+import com.alpdogan.YellowPages.dto.requestDto.SaveCategoryRequestDto;
+import com.alpdogan.YellowPages.dto.requestDto.UpdateCategoryRequestDto;
+import com.alpdogan.YellowPages.dto.responseDto.CategoryResponseDto;
+import com.alpdogan.YellowPages.entity.Category;
 import com.alpdogan.YellowPages.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/category")
@@ -12,6 +19,49 @@ public class CategoryController {
     @Autowired
     CategoryService categoryService;
 
+    @PostMapping("/saveCategory")
+    public ResponseEntity<String> saveCategory(@RequestBody SaveCategoryRequestDto saveCategoryRequestDto) {
 
+        String categorySaveDescription = categoryService.saveCategory(saveCategoryRequestDto);
+
+        return new ResponseEntity<>(categorySaveDescription, HttpStatus.OK);
+
+    }
+
+    @GetMapping("/findCategoryById")
+    public ResponseEntity<Category> findCategoryById(@RequestParam Integer categoryId) {
+
+        Category category = categoryService.findCategory(categoryId);
+
+        return new ResponseEntity<>(category, HttpStatus.OK);
+
+    }
+
+    @GetMapping("/findAllCategory")
+    public ResponseEntity<List<CategoryResponseDto>> findAllCategory() {
+
+        List<CategoryResponseDto> categoryResponseDtos = categoryService.findAllCategory();
+
+        return new ResponseEntity<>(categoryResponseDtos, HttpStatus.OK);
+
+    }
+
+    @PostMapping("/updateCategory")
+    public ResponseEntity<String> updateCategory(@RequestBody UpdateCategoryRequestDto updateCategoryRequestDto) {
+
+        String updateCategoryDescription = categoryService.updateCategory(updateCategoryRequestDto);
+
+        return new ResponseEntity<>(updateCategoryDescription, HttpStatus.OK);
+
+    }
+
+    @DeleteMapping("/deleteCategory")
+    public ResponseEntity<String> deleteCategoryById(@RequestParam Integer categoryId) {
+
+        String deleteCategoryDescription = categoryService.deleteCategoryById(categoryId);
+
+        return new ResponseEntity<>(deleteCategoryDescription, HttpStatus.OK);
+
+    }
 
 }
